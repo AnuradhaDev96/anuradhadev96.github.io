36b35b99a803b917d66eebefe243242d6343a606
AnuradhaDev96 <anusampath9470@gmail.com>
2025-06-12 21:48:15 +0530

    Project commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+// import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+// import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+// import { AppComponent } from './app.component';
+// // import { SwiperDirective } from '';
+
+// // Step 1: Add the following line...
+// import { register } from 'swiper/element/bundle';
+// import { AppRoutingModule } from './app.routes';
+
+// // Step 2: Add the following line...
+// register();
+
+// @NgModule({
+//   declarations: [
+    
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     AppComponent
+//   ],
+//   providers: [provideClientHydration()],
+//   bootstrap: [],
+
+//   // Step 3: Add the following block...
+//   schemas: [
+//     CUSTOM_ELEMENTS_SCHEMA
+//   ]
+// })
+// export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { ServiceDetailsComponent } from './views/service-details/service-details.component';
+import { PortfolioDetailsComponent } from './views/portfolio-details/portfolio-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'service-details', component: ServiceDetailsComponent},
+    {path: 'portfolio-details', component: PortfolioDetailsComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
