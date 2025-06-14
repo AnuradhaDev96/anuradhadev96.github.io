4a02e422e39a036e5a85c8e4e57e2a4e7f1844eb
AnuradhaDev96 <anusampath9470@gmail.com>
2025-06-14 09:36:00 +0530

    Routing issue fix started

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { ServiceDetailsComponent } from './views/service-details/service-details.component';
 import { PortfolioDetailsComponent } from './views/portfolio-details/portfolio-details.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
+    {path: '', component: AppComponent, pathMatch: 'full'},
     {path: 'service-details', component: ServiceDetailsComponent},
     {path: 'portfolio-details', component: PortfolioDetailsComponent},
+    {path: '**', component: PagenotfoundComponent}
 ];
 
 @NgModule({
