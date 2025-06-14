76df4d6848b3550298c34dc36d045598800ec242
AnuradhaDev96 <anusampath9470@gmail.com>
2025-06-14 10:11:31 +0530

    Corrected routing structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { ServiceDetailsComponent } from './views/service-details/service-details.component';
 import { PortfolioDetailsComponent } from './views/portfolio-details/portfolio-details.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
-import { AppComponent } from './app.component';
+import { PagenotfoundComponent } from './views/pagenotfound/pagenotfound.component';
+import { HomeComponent } from './views/home/home.component';
+import { ResumeComponent } from './views/resume/resume.component';
 
 export const routes: Routes = [
-    {path: '', component: AppComponent, pathMatch: 'full'},
+    {path: '', component: HomeComponent, pathMatch: 'full'},
     {path: 'service-details', component: ServiceDetailsComponent},
     {path: 'portfolio-details', component: PortfolioDetailsComponent},
+    {path: 'resume', component: ResumeComponent},
     {path: '**', component: PagenotfoundComponent}
 ];
 
