77e48f357ce910c6c5bda147ba09e1909ba93961
AnuradhaDev96 <anusampath9470@gmail.com>
2025-06-15 13:05:26 +0530

    Component wise fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainUiHandlerService } from './services/main-ui-handler.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,11 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  constructor(private uiHandler: MainUiHandlerService) {}
+  ngAfterViewInit(): void {
+    this.uiHandler.init();
+  }
+
   title = 'Anuradha S.';
 }
