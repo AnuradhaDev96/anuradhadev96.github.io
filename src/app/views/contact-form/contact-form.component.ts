98acf4450ed665cd6a36872468ecc67054a44919
AnuradhaDev96 <anusampath9470@gmail.com>
2025-06-23 19:58:11 +0530

    Contact us via mail

--- src/app/views/contact-form/contact-form.component.ts
+++ src/app/views/contact-form/contact-form.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { EmailRequestService } from '../../services/email-request.service';
 
 @Component({
   selector: 'app-contact-form',
@@ -10,6 +11,7 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
+  emailRequestService = inject(EmailRequestService);
   contactForm: FormGroup;
   
   loading: Boolean = false;
@@ -20,26 +22,41 @@ export class ContactFormComponent {
     this.contactForm =this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      subject: ['', Validators.required],
+      // subject: ['', Validators.required],
       message: ['', Validators.required]
     });
   }
 
-  onSubmit() {
+  async onSubmit() {
     this.contactForm.markAllAsTouched();
     if (this.contactForm.valid) {
-      console.log('Form Submitted:', this.contactForm.value);
-      // this.errorMessage = 'Please fill out all required fields correctly.';
       this.loading = true;
-        this.success = false;
-        this.errorMessage = '';
-  
-        // Simulate a service call
+      this.success = false;
+      this.errorMessage = '';
+
+
+      var result = await this.emailRequestService.sendMinimalEmailRequest(
+        this.contactForm.value.name,
+        this.contactForm.value.email,
+        this.contactForm.value.message
+      );
+
+      this.loading = false;
+
+      if (result) {
+        this.success = true;
         setTimeout(() => {
-          this.loading = false;
-          this.success = true;
-          // this.contactForm.reset();
-        }, 2000);
+          this.success = false;
+          this.errorMessage = '';
+        }, 8000);
+      } else {
+        this.errorMessage = 'Sorry! We are currently not available. Please contact via other method.'
+
+        setTimeout(() => {
+          this.success = false;
+          this.errorMessage = '';
+        }, 4000);
+      }
     } else {
       this.contactForm.markAllAsTouched();
     }
